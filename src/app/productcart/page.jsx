915dd25896d57062bbea1bf960f1c113bc0b18d6
Fawad Ahmad <fawad7998@gmail.com>
2024-10-15 17:01:43 +0500

    Feat:Some Minor Update

--- src/app/productcart/page.jsx
+++ src/app/productcart/page.jsx
@@ -31,7 +31,7 @@ const ProductCart = () => {
 
     return (
         <div className="container mx-auto p-4">
-            <div className='flex justify-center text-4xl font-bold'>Redux Toolkit</div>
+            <div className='flex justify-center text-4xl font-bold'>Redux Toolkit / Redux Persist</div>
             <div className="flex justify-between items-center mb-4">
                 <h1 className="text-2xl font-bold">Products</h1>
                 <Link href="/cart">
